aebaa6d0ed7ad9c29bca96be5ed8b8af42802bdc
Pavel <ozareniep@gmail.com>
2024-12-09 22:03:55 +0300

    current version

--- public/assets/image/model/elia/index.js
+++ public/assets/image/model/elia/index.js
@@ -4,7 +4,6 @@ export { default as image2 } from './2.jpg';
 export { default as image3} from './3.jpg';
 export { default as image4 } from './4.jpg';
 export { default as image5 } from './5.jpg';
-export { default as image6 } from './6.jpg';
 export { default as image7 } from './7.jpg';
 export { default as image8 } from './8.jpg';
 export { default as image9 } from './9.jpg';
